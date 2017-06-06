b296975795c40bac4a7e619a737bbbefb7187756
Alexey Kuznetsov <kuznetsov@mystand.ru>
2017-06-07 03:16:55 +0500

    removed puts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
-    request.headers.each { |key, _value| puts key }
     team = Team.find_or_create_from_slack auth
     team.export!
     redirect_to team_url(team.id)
