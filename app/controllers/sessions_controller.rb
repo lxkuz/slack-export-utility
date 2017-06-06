b222f5ad93287455561913fbc058f84e8b4d5852
Alexey Kuznetsov <kuznetsov@mystand.ru>
2017-06-06 23:30:47 +0500

    remove session slack id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,6 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     team = Team.find_or_create_from_slack auth
     team.export!
-    session[:team] = team.slack_id
     redirect_to team_url(team.id)
-  end
-
-  def destroy
-    session[:team] = nil
-    redirect_to root_url
   end
 end
